88d5ef8bce40ca781fa4bdfa2da8a70c716628b7
LSA88 <lovervz@naver.com>
2022-11-03 10:07:24 +0900

    Event Delegation

--- main.js
+++ main.js
@@ -20,33 +20,19 @@ function onAdd() {
   input.focus();
 }
 
+id = 0;
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const itemName = document.createElement("span");
-  itemName.setAttribute("class", "item__name");
-  itemName.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(itemName);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+      <div class = "item">
+        <span class = "item__name">${text}</span>
+        <button class = "item__delete">
+          <i class="fa-solid fa-trash-can" data-id=${id}></i>
+        </button>
+      </div>
+      <div class="item__divider"></div>`;
   return itemRow;
 }
 
@@ -57,5 +43,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", event => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", event => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
